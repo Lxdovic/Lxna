dd54889b365aa8a7413d7ebc340b80267b74223d
Lxdovic <ludovicdebever0@gmail.com>
2023-10-22 12:01:27 +0200

    feat(UCI): Started implementing UCI

--- Lxna/Engine.cs
+++ Lxna/Engine.cs
@@ -95,21 +95,18 @@ namespace Lxna {
     }
     
     internal class Engine {
-
-        public static int nodes = 0;
-        public static Board board;
+        public static int nodes;
         public static readonly String EMPTY_BOARD = "8/8/8/8/8/8/8/8 b - - ";
         public static readonly String TRICKY_POS = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ";
         public static readonly String START_POS = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ";
         public static readonly String KILLER_POS = "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1";
         public static readonly String CMK_POS = "r2q1rk1/ppp2ppp/2n1bn2/2b1p3/3pP3/3P1NPP/PPP1NPB1/R1BQ1RK1 b - - 0 9 ";
 
+        public static Board board = new Board(START_POS);
+
         public static void Main(string[] args) {
             Movegen.Init();
-            board = new Board(TRICKY_POS);
-            
-            PerfTest(5);
-
+            UniversalChessInterface.StartLoop();
         }
 
         public static void PerfTest(int depth) {
